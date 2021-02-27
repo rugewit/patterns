637954f17b924286fc5c8a143712f62c267ae710
rugewit <rugewit@yandex.ru>
2021-02-27 22:40:42 +0300

    factry method c++ clion

---
+++ Factory Method C++ Clion/cmake-build-debug/CMakeFiles/Factory_Method_C___Clion.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Factory_Method_C___Clion.dir/main.cpp.o"
+  "Factory_Method_C___Clion"
+  "Factory_Method_C___Clion.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Factory_Method_C___Clion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
